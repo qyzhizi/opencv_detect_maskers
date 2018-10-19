4f3788342de2afe69432a3fa2db18ee44bfdbf48
qy <l2830942138@gmail.com>
2018-10-19 18:30:28 +0800

    first commit

---
+++ CMakeFiles/only_detect_markers_exe.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/qy/software/opencv_program/opencv_marker/detect_markers/only_detect_markers.cpp" "/home/qy/software/opencv_program/opencv_marker/detect_markers/CMakeFiles/only_detect_markers_exe.dir/only_detect_markers.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/opencv34/include"
+  "/usr/opencv34/include/opencv"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/only_detect_markers_exe.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/only_detect_markers_exe.dir/only_detect_markers.cpp.o"
+  "only_detect_markers_exe.pdb"
+  "only_detect_markers_exe"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/only_detect_markers_exe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
